d42630355e1bca5156547f3823baf9e15df799c6
Rxu64 <wx2006@nyu.edu>
2022-10-27 11:31:24 -0400

    sudo code added for perform

--- replica/main.go
+++ replica/main.go
@@ -19,17 +19,17 @@ const (
 )
 
 var (
-	replicaId int32
-	server    *grpc.Server
-
-	state        string
-	slot_in      int32 = 1
-	slot_out     int32 = 1
-	requests     []*pb.Command
-	proposals    []*pb.Proposal
-	decisions    []*pb.Decision
-	leaderPorts  = []string{"127.0.0.1:50055", "127.0.0.1:50056"}
+	server       *grpc.Server
+	replicaId    int32
 	replicaPorts = []string{"127.0.0.1:50053", "127.0.0.1:50054"}
+
+	state       string
+	slot_in     int32 = 1
+	slot_out    int32 = 1
+	requests    [leaderNum]chan *pb.Command
+	proposals   [leaderNum]chan *pb.Proposal
+	decisions   chan *pb.Decision
+	leaderPorts = []string{"127.0.0.1:50055", "127.0.0.1:50056"}
 
 	responses []*pb.Response
 )
@@ -78,20 +78,60 @@ func MessengerRoutine(serial int) {
 
 	c := pb.NewReplicaLeaderClient(conn)
 
-	// reference sudo code
-	for slot_in < slot_out+WINDOW && len(requests) != 0 {
+	// reference sudo code propose()
+	for slot_in < slot_out+WINDOW {
+		request := <-requests[serial]
+		proposals[serial] <- &pb.Proposal{SlotNumber: slot_in, Command: request}
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 		defer cancel()
-		c.Propose(ctx, &pb.Proposal{SlotNumber: slot_in, Command: nil})
+		c.Propose(ctx, &pb.Proposal{SlotNumber: slot_in, Command: request})
+		slot_in++
 	}
 }
 func CollectorRoutine(serial int) {
+	conn, err := grpc.Dial(leaderPorts[serial], grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Printf("failed to connect: %v", err)
+		return
+	}
+	defer conn.Close()
 
+	c := pb.NewReplicaLeaderClient(conn)
+
+	// reference sudo code perform()
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		r, err := c.Collect(ctx, &pb.Empty{Content: "checking responses"})
+		if err != nil {
+			log.Printf("failed to collect: %v", err)
+		}
+		if r.Valid {
+			for _, decision := range r.Decisions {
+				decisions <- decision
+				// sudo code
+				for _, proposal := range proposals {
+					if decision.SlotNumber == proposal.SlotNumber {
+						proposals.remove(proposal)
+						if decision.Command != proposal.Command {
+							requests[serial] <- proposal
+						}
+					}
+				}
+				// perform(decision) atomically
+				state = state + decision.Command.Operation
+				slot_out++
+				log.Printf("Operation %s is performed", decision.Command.Operation)
+			}
+		}
+	}
 }
 
 // handlers
 func (s *replicaServer) Request(ctx context.Context, in *pb.Command) (*pb.Empty, error) {
-	requests = append(requests, in)
+	for i := 0; i < leaderNum; i++ {
+		requests[i] <- in
+	}
 	return &pb.Empty{Content: "success"}, nil
 }
 func (s *replicaServer) Collect(ctx context.Context, in *pb.Empty) (*pb.Responses, error) {
