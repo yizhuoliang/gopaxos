1959efcc2d6c1b0ff395b99a5f9839e969951674
Yizhuo-Liang <yizhuo.liang.hello@gmail.com>
2022-10-31 03:36:41 -0400

    leader finished half

--- leader/main.go
+++ leader/main.go
@@ -1,28 +1,115 @@
 package main
 
 import (
+	"context"
 	pb "gopaxos/gopaxos"
+	"log"
+	"net"
+	"os"
+	"strconv"
 
 	"google.golang.org/grpc"
 )
 
-const (
-	replicaNum = 3
-	WINDOW     = 5
-)
-
 var (
-	server       *grpc.Server
-	replicaId    int32
-	replicaPorts = []string{"127.0.0.1:50053", "127.0.0.1:50054"}
+	server   *grpc.Server
+	leaderId int32 // also considered as the serial of this acceptor
 
-	state       string
-	slot_in     int32 = 1
-	slot_out    int32 = 1
-	requests    [leaderNum]chan *pb.Command
-	proposals   [leaderNum]chan *pb.Proposal
-	decisions   chan *pb.Decision
-	leaderPorts = []string{"127.0.0.1:50055", "127.0.0.1:50056"}
+	leaderPorts   = []string{"127.0.0.1:50055", "127.0.0.1:50056"}
+	acceptorPorts = []string{"127.0.0.1:50057", "127.0.0.1:50058", "127.0.0.1:50059"}
 
-	responses []*pb.Response
+	// leader states
+	ballotNumber int32
+	active       bool = false
+	proposals    map[int32]*pb.Proposal
+
+	ballotNumberUpdateChannel chan int32
+	proposalsUpdateChannel    chan *pb.Proposal
+
+	decisions []*pb.Decision
+
+	feedbackChannel chan int32
 )
+
+type leaderServer struct {
+	pb.UnimplementedReplicaLeaderServer
+}
+
+func main() {
+	temp, _ := strconv.Atoi(os.Args[1])
+	leaderId = int32(temp)
+
+	ballotNumberUpdateChannel = make(chan int32)
+	go ballotNumberUpdateRoutine()
+
+	feedbackChannel = make(chan int32)
+	// TODO: implement feedback, need a struct for Preemption and adoption
+	// TODO: update decisions[] during adoption for sending feedback to replicas
+
+	serve(leaderPorts[leaderId])
+
+	// TODO: more leader routine
+}
+
+func serve(port string) {
+	// listen client on port
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Printf("server listening at %v", lis.Addr())
+	server = grpc.NewServer()
+	pb.RegisterReplicaLeaderServer(server, &leaderServer{})
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+// UPDATER ROUTINES
+func ballotNumberUpdateRoutine() {
+	for {
+		newNum := <-ballotNumberUpdateChannel
+
+		// CONCERN: need more consideration on the concurrency of ballotNumber
+		if newNum > ballotNumber {
+			ballotNumber = newNum
+		}
+	}
+}
+
+// TODO: expand this
+func proposalsUpdateRoutine() {
+	for {
+		newProposal := <-proposalsUpdateChannel
+		if _, ok := proposals[newProposal.SlotNumber]; !ok {
+			proposals[newProposal.SlotNumber] = newProposal
+			go CommanderRoutine(&pb.BSC{
+				BallotNumber: ballotNumber, SlotNumber: newProposal.SlotNumber, Command: newProposal.Command})
+		}
+	}
+}
+
+// SUB-ROUTINES
+func ScoutRoutine() {
+
+}
+
+func CommanderRoutine(bsc *pb.BSC) {
+
+}
+
+// gRPC HANDLERS
+func (s *leaderServer) Propose(ctx context.Context, in *pb.Proposal) (*pb.Empty, error) {
+	proposalsUpdateChannel <- in // Weird? Yes! Things can only be down after chekcing proposals
+	return &pb.Empty{Content: "success"}, nil
+}
+
+func (s *leaderServer) Collect(ctx context.Context, in *pb.Empty) (*pb.Decisions, error) {
+	if len(decisions) == 0 {
+		return &pb.Decisions{Valid: false, Decisions: nil}, nil
+	} else {
+		r := decisions
+		decisions = decisions[:0]
+		return &pb.Decisions{Valid: true, Decisions: r}, nil
+	}
+}
