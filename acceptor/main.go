73304a243b9c8958c55f74a945d3f5c16fddf8cc
yizhuoliang <yizhuo.liang.hello@gmail.com>
2022-11-04 17:07:30 -0400

    acceptor mutex added, protofile update

--- acceptor/main.go
+++ acceptor/main.go
@@ -17,22 +17,26 @@ var (
 	acceptorPorts       = []string{"127.0.0.1:50057", "127.0.0.1:50058", "127.0.0.1:50059"}
 
 	ballotNumber int32 = -1
+	ballotLeader int32 = -1
 	accepted     map[int32][]*pb.BSC
 
-	ballotNumberUpdateChannel chan int32
+	mutexChannel chan int32
 )
 
 type acceptorServer struct {
 	pb.UnimplementedLeaderAcceptorServer
 }
 
+type acceptorStateUpdate struct {
+	newNumber int32
+	newLeader int32
+}
+
 func main() {
 	temp, _ := strconv.Atoi(os.Args[1])
 	acceptorId = int32(temp)
 
-	ballotNumberUpdateChannel = make(chan int32, 1)
-	go ballotNumberUpdateRoutine()
-
+	mutexChannel = make(chan int32, 1)
 	serve(acceptorPorts[acceptorId])
 }
 
@@ -50,33 +54,31 @@ func serve(port string) {
 	}
 }
 
-func ballotNumberUpdateRoutine() {
-	for {
-		newNum := <-ballotNumberUpdateChannel
-
-		// CONCERN: need more consideration on the concurrency of ballotNumber
-		if newNum > ballotNumber {
-			ballotNumber = newNum
-		}
-	}
-}
-
 // gRPC handlers
 func (s *acceptorServer) Scouting(ctx context.Context, in *pb.P1A) (*pb.P1B, error) {
-	ballotNumberUpdateChannel <- in.BallotNumber // considerations on why not check her
+	<-mutexChannel
+	if in.BallotNumber > ballotNumber || (in.BallotNumber == ballotNumber && in.LeaderId != ballotLeader) {
+		ballotNumber = in.BallotNumber
+		ballotLeader = in.LeaderId
+	}
 	var acceptedList []*pb.BSC
 	for i := 1; i < int(ballotNumber); i++ {
 		if bscs, ok := accepted[int32(i)]; ok {
 			acceptedList = append(acceptedList, bscs...)
 		}
 	}
+	mutexChannel <- 1
 	return &pb.P1B{AcceptorId: acceptorId, BallotNumber: ballotNumber, Accepted: acceptedList}, nil
 }
 
 func (s *acceptorServer) Commanding(ctx context.Context, in *pb.P2A) (*pb.P2B, error) {
-	currentBallotNumber := ballotNumber // concurrency concern, avoid ballot number update during execution
-	if in.Bsc.BallotNumber == currentBallotNumber {
-		accepted[currentBallotNumber] = append(accepted[currentBallotNumber], in.Bsc)
+	<-mutexChannel
+	ballotNumber := ballotNumber // concurrency concern, avoid ballot number update during execution
+	if in.Bsc.BallotNumber == ballotNumber && in.LeaderId == ballotLeader {
+		accepted[ballotNumber] = append(accepted[ballotNumber], in.Bsc)
 	}
-	return &pb.P2B{AcceptorId: acceptorId, BallotNumber: currentBallotNumber}, nil
+	currentBallotNumber := ballotNumber
+	currentBallotLeader := ballotLeader
+	mutexChannel <- 1
+	return &pb.P2B{AcceptorId: acceptorId, BallotNumber: currentBallotNumber, BallotLeader: currentBallotLeader}, nil
 }
