af68f4404f493a043713c0ce61bc6a17785d5574
yizhuoliang <yizhuo.liang.hello@gmail.com>
2022-11-05 17:02:28 -0400

    update

--- client/main.go
+++ client/main.go
@@ -96,7 +96,7 @@ func CollectorRoutine(serial int) {
 	c := pb.NewClientReplicaClient(conn)
 
 	for {
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 		defer cancel()
 		r, err := c.Collect(ctx, &pb.Empty{Content: "checking responses"})
 		if err != nil {
