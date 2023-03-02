0d846d9505d22b46d60dde04f9706aa4d476199e
Rxu64 <wx2006@nyu.edu>
2023-03-02 16:32:28 -0500

    Linearizable reads stable version

--- replica/main.go
+++ replica/main.go
@@ -174,7 +174,6 @@ func ReplicaStateUpdateRoutine() {
 			}
 		} else if update.updateType == 2 {
 			// PROPOSE NEW COMMAND
-			log.Printf("processing new request...")
 			// reference sudo code propose()
 			if slot_in < slot_out+WINDOW {
 				_, ok := decisions[slot_in]
