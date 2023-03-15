642401cafcf60de957c257d3f82af2672091c23a
yizhuoliang <yizhuo.liang.hello@gmail.com>
2023-03-15 11:19:26 -0400

    fix replyChann

--- paxosClient/client.go
+++ paxosClient/client.go
@@ -62,6 +62,7 @@ func NewPaxosClient(clientId int, simon int) *Client {
 	// initialize command channels for messenger routines
 	for i := 0; i < replicaNum; i++ {
 		client.messageChannels[i] = make(chan *pb.Message, 1)
+		client.replyChannels[i] = make(chan *reply)
 	}
 
 	// launch messenger and collector routines
